e732f4f77ca8d5f40ffc07a0c2630a05cdde70a5
Fabien Winkler <fabien.winkler@outlook.fr>
2023-04-26 13:31:30 +0200

    fix #31

--- src/spectrum.js
+++ src/spectrum.js
@@ -1846,10 +1846,10 @@
         function rgbaToHex(r, g, b, a) {
 
             var hex = [
-                pad2(convertDecimalToHex(a)),
-            pad2(mathRound(r).toString(16)),
-            pad2(mathRound(g).toString(16)),
-            pad2(mathRound(b).toString(16))
+                pad2(mathRound(r).toString(16)),
+                pad2(mathRound(g).toString(16)),
+                pad2(mathRound(b).toString(16)),
+                pad2(convertDecimalToHex(a))
         ];
 
         return hex.join("");
